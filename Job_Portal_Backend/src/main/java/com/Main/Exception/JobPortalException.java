2eae483245a9e0440377cd873c5c33ca233329b0
Angel <Harry>
2025-04-17 18:31:21 +0530

    Updated new branch commit

---
+++ Job_Portal_Backend/src/main/java/com/Main/Exception/JobPortalException.java
@@ -0,0 +1,10 @@
+package com.Main.Exception;
+
+public class JobPortalException extends Exception{
+
+	public static final long serialVersionUID=1L;
+	
+	public JobPortalException(String msg) {
+		super(msg);
+	}
+}
