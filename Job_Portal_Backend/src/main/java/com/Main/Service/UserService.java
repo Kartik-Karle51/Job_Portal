8d4f9e1c1cf96279273e6d6efd676c469f14cd95
Angel <Harry>
2025-04-28 21:33:05 +0530

    Committed By Dipali

--- Job_Portal_Backend/src/main/java/com/Main/Service/UserService.java
+++ Job_Portal_Backend/src/main/java/com/Main/Service/UserService.java
@@ -3,10 +3,7 @@ package com.Main.Service;
 import org.springframework.stereotype.Repository;
 
 import com.Main.DTO.LoginDTO;
-<<<<<<< HEAD
 import com.Main.DTO.ResponseDTO;
-=======
->>>>>>> 99166604a113c2ad9c6d83b6aed3b7323d6bd303
 import com.Main.DTO.UserDTO;
 import com.Main.Exception.JobPortalException;
 
@@ -16,7 +13,6 @@ import jakarta.validation.Valid;
 public interface UserService {
 	
 	public UserDTO registerUser(UserDTO userDTO) throws Exception;
-<<<<<<< HEAD
 
 	public UserDTO loginUser(LoginDTO loginDTO) throws JobPortalException;
 	
@@ -25,9 +21,7 @@ public interface UserService {
 	public boolean verifyOtp(String email,String otp) throws JobPortalException;
 
 	public ResponseDTO changePassword( LoginDTO loginDTO) throws JobPortalException;
-=======
 	public UserDTO getUserByEmail(String email) throws JobPortalException;
 
-	public UserDTO loginUser(LoginDTO loginDTO) throws JobPortalException;
->>>>>>> 99166604a113c2ad9c6d83b6aed3b7323d6bd303
+
 }
